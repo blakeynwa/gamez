500cbe6c71cf43742c0c3c728c5c94b1be63308d
Blake Meyer <blake.ynwa@gmail.com>
2017-08-11 15:52:22 -0700

    Build out user controller

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,9 +1,25 @@
-get '/users'  do
-  @users = User.all
-  erb :users
+# get '/users'  do
+#   @users = User.all
+#   erb :'/users/users'
+# end
+
+# get '/users/:id' do
+#   @users = User.find(params[:id])
+#   erb :'/users/one_user'
+# end
+
+get '/users/new' do
+  erb :'/users/create_user'
 end
 
-get '/users/:id' do
-  @users = User.find(params[:id])
-  erb :one_user
+post '/users' do
+  redirect 'users/stats'
+end
+
+get '/users/login' do
+  erb :'/users/login_page'
+end
+
+get '/users/stats' do
+  erb :'/users/user_stats'
 end
