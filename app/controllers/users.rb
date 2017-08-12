eb9a282765bbb6715169007a09004943dd7deaf2
Blake Meyer <blake.ynwa@gmail.com>
2017-08-11 17:26:40 -0700

    Build out ability for users to create an account and login

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,15 +1,14 @@
-# get '/users'  do
-#   @users = User.all
-#   erb :'/users/users'
-# end
-
-# get '/users/:id' do
-#   @users = User.find(params[:id])
-#   erb :'/users/one_user'
-# end
-
 get '/users/new' do
   erb :'/users/create_user'
+end
+
+post '/users' do
+  user_info = params[:user]
+  user = User.new(username: user_info[:username], email: user_info["email"])
+  user.password = user_info[:password]
+  user.save
+  session[:user_id] = user.id
+  redirect 'users/stats'
 end
 
 post '/users' do
@@ -20,6 +19,18 @@ get '/users/login' do
   erb :'/users/login_page'
 end
 
+post '/login' do
+  user = User.authenticate(params[:user][:username], params[:user][:password])
+  session[:user_id] = user.id
+  redirect 'users/stats'
+end
+
 get '/users/stats' do
   erb :'/users/user_stats'
 end
+
+post '/logout' do
+  session.delete(:user_id)
+  redirect '/'
+end
+
--- db/migrate/20170811160327_delete_deck_from_round.rb
+++ db/migrate/20170811160327_delete_deck_from_round.rb
@@ -1,5 +1,5 @@
 class DeleteDeckFromRound < ActiveRecord::Migration[5.1]
   def change
-    remove_column :rounds, :deck_id
+    remove_column :rounds, :deck_id, :integer
   end
 end
