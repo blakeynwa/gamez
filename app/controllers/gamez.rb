86a77562c3f72b1be34c38859ffda9ff359d08f7
Emmet Mahdavi <emmetrm@gmail.com>
2017-08-11 19:05:20 -0700

    add functioning selection to home when choosing quiz
    need to change methods in round class, http is stateless, I designed the model without that in mind. it would work to write a helper method that checks which cards in the game do not have a correct guess obj pointing at them.

--- app/controllers/gamez.rb
+++ app/controllers/gamez.rb
@@ -3,16 +3,34 @@ get '/gamez' do
   erb :'gamez/gamez_home'
 end
 
-get '/gamez/id' do
+get '/gamez/:id' do
+  # if logged_in?
+  #   session[:round_id] ||= Round.create(user_id: session[:user_id],deck_id: params[:id]).id
+  # else
+  #   session[:round_id] ||= Round.create(user_id: 1,deck_id: params[:id]).id
+  # end
+  @title = Deck.find(params[:id]).title
+  @round = Round.find(session[:round_id])
+  @card = @round.next_card
+
   erb :'/gamez/display_question'
 end
 
-post '/gamez/id' do
-  #session[:round_id] = params[]
-  #redirect '/gamez/id' unless !Deck.question.any? #logic to redirect only if questions remain
-  redirect '/gamez/id/stats'
-end
+# post '/gamez/:id' do
+
+#   #redirect '/gamez/id' unless !Deck.question.any? #logic to redirect only if questions remain
+#   redirect '/gamez/id/stats'
+# end
 
 get '/gamez/id/stats' do
   erb :'/gamez/stats'
+end
+
+post '/gamez/:id' do
+  if logged_in?
+    session[:round_id] ||= Round.create(user_id: session[:user_id]).id
+  else
+    session[:round_id] ||= Round.create(user_id: 1).id
+  end
+  redirect "/gamez/#{params[:id]}"
 end
--- app/helpers/utility.rb
+++ app/helpers/utility.rb
@@ -2,4 +2,6 @@ helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
+
+
 end
